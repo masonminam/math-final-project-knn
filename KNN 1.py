8aedec10c6af0b49e8d27a81467515f0382d8ec1
masonminam <75981518+masonminam@users.noreply.github.com>
2020-12-14 22:36:00 +0700

    Add files via upload

---
+++ KNN 1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 23:21:05 2020
+
+@author: mason
+"""# Make Predictions with k-nearest neighbors on the Movie Recommendation System Dataset
+from csv import reader
+from math import sqrt
+
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+		print('[%s] => %d' % (value, i))
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
+
+# Calculate the Euclidean distance between two vectors
+def euclidean_distance(row1, row2):
+	distance = 0.0
+	for i in range(len(row1)-1):
+		distance += (row1[i] - row2[i])**2
+	return sqrt(distance)
+
+# Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+	distances = list()
+	for train_row in train:
+		dist = euclidean_distance(test_row, train_row)
+		distances.append((train_row, dist))
+	distances.sort(key=lambda tup: tup[1])
+	neighbors = list()
+	for i in range(num_neighbors):
+		neighbors.append(distances[i][0])
+	return neighbors
+
+# Make a prediction with neighbors
+def predict_classification(train, test_row, num_neighbors):
+	neighbors = get_neighbors(train, test_row, num_neighbors)
+	output_values = [row[-1] for row in neighbors]
+	prediction = max(set(output_values), key=output_values.count)
+	return prediction
+ 
+# Make a prediction with KNN on the Movie Recommendation System Dataset
+filename = 'trainingdata.csv'
+
+dataset = load_csv(filename)
+
+for i in range(len(dataset[0])-1):
+
+	str_column_to_int(dataset, i)
+
+# Define the number of nearest neighbors
+num_neighbors = 2
+
+# Define a test set (Ken's previous movie preferences)
+testset = [1, 1, 0, 1]
+
+# Predict the label
+label = predict_classification(dataset, testset, num_neighbors)
+print('Data=%s, Predicted: %s' % (testset, label))
